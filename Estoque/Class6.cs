95c719d0ac0f7ebc2c31b421b52662d96f225a67
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-04-26 17:53:41 -0300

    inicio da criacao do estoque

---
+++ Estoque/Class6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Estoque
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
+
---
+++ Estoque/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Estoque
+{
+    class Program 
+    { 
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(); 
+            Console.WriteLine("Coloque os dados do produto");
+            Console.WriteLine("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+        } 
+    }
+}
